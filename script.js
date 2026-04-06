8e59aed248fda94791aeba7281cf876802805726
Timothée Poux <timothee.poux@gmail.com>
2026-04-06 18:33:04 +0200

    tendances du jour

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const apiPopulars = 'https://api.themoviedb.org/3/movie/popular';
+const apiTrendingsDay = 'https://api.themoviedb.org/3/trending/all/day';
+const apiTrendingsWeek = 'https://api.themoviedb.org/3/trending/all/week';
 let tendances = document.querySelector('#tendances');
 
 const options = {
@@ -9,14 +11,14 @@ const options = {
   }
 };
 
-fillMoviesList('https://api.themoviedb.org/3/movie/popular', tendances);
-
 const populars = await fetch(apiPopulars, options)
   .then(res => res.json())
   .catch(err => console.error(err));
 
 let search = document.querySelector('#search');
 search.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${populars.results[0].backdrop_path})`;
+
+fillMoviesList(apiTrendingsDay, tendances);
 
 function numberToMonth(nb){
   switch (nb) {
@@ -27,7 +29,7 @@ function numberToMonth(nb){
     case '03':
       return 'mars'
     case '04':
-      return 'avri'
+      return 'avr'
     case '05':
       return 'mai'
     case '06':
@@ -35,7 +37,7 @@ function numberToMonth(nb){
     case '07':
       return 'juil'
     case '08':
-      return 'aoüt'
+      return 'août'
     case '09':
       return 'sept'
     case '10':
@@ -55,17 +57,24 @@ function prettyDate(date){
 
 async function fillMoviesList(apiLink, section){
   const data = await fetch(apiLink, options)
-  .then(res => res.json())
-  .catch(err => console.error(err));
+    .then(res => res.json())
+    .catch(err => console.error(err));
+  console.log('Données récupérées :', data);
 
   let poster = section.querySelectorAll('img');
   let title = section.querySelectorAll('.movieTitle');
   let date = section.querySelectorAll('.movieDate');
   let note = section.querySelectorAll('.movieNote');
   for (let i = 0; i<4; i++){
-  poster[i].src = `https://image.tmdb.org/t/p/original${data.results[i].poster_path}`;
-  title[i].innerHTML = data.results[i].title;
-  date[i].innerHTML = prettyDate(data.results[i].release_date);
-  note[i].innerHTML = `${Math.round(data.results[i].vote_average*10)}%`;
+    poster[i].src = `https://image.tmdb.org/t/p/original${data.results[i].poster_path}`;
+    note[i].innerHTML = `${Math.round(data.results[i].vote_average*10)}%`;
+    if (data.results[i].media_type == "movie"){
+      title[i].innerHTML = data.results[i].title;
+      date[i].innerHTML = prettyDate(data.results[i].release_date);
+    }
+    else if (data.results[i].media_type == "tv"){
+      title[i].innerHTML = data.results[i].name;
+      date[i].innerHTML = prettyDate(data.results[i].first_air_date);
+    }
   }
 }
